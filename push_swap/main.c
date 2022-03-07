57d816020e83ac70fd8c9aaab9806f09d6a225f8
Randlii <93086288+Randlii@users.noreply.github.com>
2022-03-07 17:33:13 +0300

    Initial Commit

---
+++ push_swap/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  push_swap
+//
+//  Created by Lincoln Graceford on 3/7/22.
+//  Copyright © 2022 Linoln Graceford. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
